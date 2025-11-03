533a1c2f5c65094444a8d08f6e2aceb9fcbe45f2
Tamar Brenig <42570337+cx-tamar-brenig@users.noreply.github.com>
2025-11-03 11:53:14 +0200

    Update featureA.cs

--- featureA.cs
+++ featureA.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 
+
+
+
 namespace SQLi_1
 {
     class Program
@@ -42,6 +45,10 @@ namespace SQLi_1
                         cmd.Connection = conn;
                         cmd.ExecuteScalar();
                     }
+
+
+
+                    
 
                 }
             }
